70d994f569e4d8f54607d3b7264da77c3275a917
jesutin <jesutin91@gmail.com>
2018-03-24 09:58:45 +0100

    L'installation de vue et la récuperation des données

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <div class="post" v-for="post in posts" :key="post.id">
+     <h3>Title: {{ post.title}} </h3>
+     <P>Description:{{ post.body}}</p>
+  </div>
+  </div>
+</template>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<script>
+import axios from 'axios';
+export default {
+  data () {
+    return {
+      posts: []
+    }
+  },
+  mounted()
+  {var self = this;
+     axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(function(res){
+       self.posts = res.data;
+       console.log('Data:',res.data);
+      })
+     .catch(function(error){
+       console.log('Error:',error);
+      } )
+  }
+}
+</script>
